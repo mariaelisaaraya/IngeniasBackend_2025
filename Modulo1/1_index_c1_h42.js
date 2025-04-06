b37428a71cea0b7d5bad49b635ff668a4baa60c0
Maria Elisa Araya <42949801+mariaelisaaraya@users.noreply.github.com>
2025-04-06 18:54:52 -0300

    console.log

---
+++ Modulo1/1_index_c1_h42.js
@@ -0,0 +1,7 @@
+//https://www.aprendejavascript.dev/clase/introduccion/console-log refuerzo de console.log
+/*console.log() es una función integrada en JavaScript que se utiliza para imprimir mensajes en la consola del navegador o del editor de código. Se utiliza principalmente para depurar el código y para imprimir valores de variables y mensajes para ayudar en el proceso de desarrollo. */
+
+//correr con node 1_index_c1_h42.js
+//ppt clase1 hoja 42
+
+console.log("Hola Mundo!");
---
+++ Modulo1/2_index_c1_h46.js
@@ -0,0 +1,10 @@
+//console log
+//Si alguien quiere profundizar: https://developer.mozilla.org/es/docs/Web/API/console
+//correr con node 2_index_c1_h46
+
+console.log("Esto es un mensaje comuncito");
+console.warn("Esto es un mensaje de advertencia 0.0");
+console.error("Esto es un mensaje de error!");
+
+const ferreteria = ["tornilo", "clavos", "martillo"];
+console.table(ferreteria);
