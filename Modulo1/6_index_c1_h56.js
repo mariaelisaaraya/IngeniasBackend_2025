55fa53ca61443aff506a88cf25a328fad9c82345
Maria Elisa Araya <42949801+mariaelisaaraya@users.noreply.github.com>
2025-04-06 22:28:13 -0300

    condicionales

---
+++ Modulo1/6_index_c1_h56.js
@@ -0,0 +1,7 @@
+const nombreCompleto = "Maria Elisa";
+
+if(nombreCompleto !== ''){
+    console.log('Bienvenida', `${nombreCompleto}`);
+}else {
+    console.error('No se reconoce a la usuaria o la usuaria esta vacia')
+}
